a9183a26b0c3f272bdc3c3eda057c996f3256b8d
Personal Computer <Personal Computer@Mich>
2024-12-07 13:00:36 +0800

    Second Commit

--- src/test/java/acceptanceTest/RunnerTest.java
+++ src/test/java/acceptanceTest/RunnerTest.java
@@ -1,5 +1,25 @@
 package acceptanceTest;
 
-public class RunnerTest {
+import org.junit.runner.RunWith;
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		monochrome = true,
+		plugin = {
+				"pretty",
+				"html:target/cucumber-report.html",
+				"json:target/cucumber-json/cucumber.json",
+				"junit:target/cucumber.xml"
+				},
+		features = {
+				"src/test/java/acceptanceTest/features"
+		},
+		glue = {
+				"com.matt.stepDef"
+		},
+		tags = "@Test")
+public class RunnerTest {
 }
