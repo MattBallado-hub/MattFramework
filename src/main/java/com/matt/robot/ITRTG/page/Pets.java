5865fe96fa6ef8e993ff24eb8e1b862f3f3fb5c2
Matt Ballado <bryonballado@gmail.com>
2024-12-31 04:36:14 +0800

    Update Pets.java

--- src/main/java/com/matt/robot/ITRTG/page/Pets.java
+++ src/main/java/com/matt/robot/ITRTG/page/Pets.java
@@ -7,7 +7,9 @@ public class Pets extends ITRTGTest {
 	private static int pets_x = 638, pets_y = 141;
 	private static int petCampaign_x = 730, petCampaign_y = 141;
 	private static int petDungeon_x = 820, petDungeon_y = 141;
-	
+
+	private static int growth_x = 982, growth_y = 392;
+
 	private static int back_x = 1665, back_y = 258;
 	private static int halfStats_x = 1758, halfStats_y = 329;
 	private static int spread_x = 1560, spread_y = 258;
@@ -17,7 +19,7 @@ public class Pets extends ITRTGTest {
 	private static int feed_x = 958, feed_y = 775;
 	private static int close_x = 928, close_y = 965;
 	private static int ok_x = 1764, ok_y = 692;
-	
+
 	private static int dungeon_x = 823, dungeon_y = 257;
 
 	public static void petLevel() {
@@ -29,11 +31,13 @@ public class Pets extends ITRTGTest {
 
 	public static void petCampaign() {
 		mouse.click(petCampaign_x, petCampaign_y);
-		mouse.click(top_x, top_y);
-		mouse.click(restartAll_x, restartAll_y);
-		mouse.click(feed_x, feed_y);
-		mouse.click(close_x, close_y);
-		mouse.click(ok_x, ok_y);
+		if (mouse.getRed(growth_x, growth_y) == 40 && mouse.getGreen(growth_x, growth_y) == 120 && mouse.getBlue(growth_x, growth_y) == 40) {
+			mouse.click(top_x, top_y);
+			mouse.click(restartAll_x, restartAll_y);
+			mouse.click(feed_x, feed_y);
+			mouse.click(close_x, close_y);
+			mouse.click(ok_x, ok_y);
+		}
 	}
 
 	public static void petDungeon() {
